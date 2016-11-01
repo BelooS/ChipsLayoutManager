837134e713ad78dd1f8db2bc0a30f9d3d2d00f3d
Beloo <Awful.Vi@yandex.ru>
2016-11-01 11:54:39 +0200

    * layouter factories small ref

--- ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/layouter/RTLLayouterFactory.java
+++ ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/layouter/RTLLayouterFactory.java
@@ -12,14 +12,18 @@ public class RTLLayouterFactory extends AbstractLayouterFactory {
         super(cacheStorage, layoutManager);
     }
 
-    public ILayouter getUpLayouter(@Nullable Rect anchorRect) {
-        Rect offsetRect = new Rect(
+
+    private Rect createOffsetRectForUpLayouter(Rect anchorRect) {
+        return new Rect(
                 //we shouldn't include anchor view here, so anchorLeft is a rightOffset
                 anchorRect == null ? layoutManager.getPaddingLeft() : anchorRect.right,
                 anchorRect == null ? layoutManager.getPaddingTop() : anchorRect.top,
                 0,
-                anchorRect == null ? layoutManager.getPaddingBottom() : anchorRect.bottom
-        );
+                anchorRect == null ? layoutManager.getPaddingBottom() : anchorRect.bottom);
+    }
+
+    public ILayouter getUpLayouter(@Nullable Rect anchorRect) {
+        Rect offsetRect = createOffsetRectForUpLayouter(anchorRect);
 
         AbstractLayouter layouter = new RTLUpLayouter(layoutManager,
                 layoutManager.getChildGravityResolver(),
@@ -31,13 +35,16 @@ public class RTLLayouterFactory extends AbstractLayouterFactory {
         return layouter;
     }
 
-    public ILayouter getDownLayouter(@Nullable Rect anchorRect) {
-        Rect offsetRect = new Rect(
+    private Rect createOffsetRectForDownLayouter(Rect anchorRect) {
+        return new Rect(
                 0,
                 anchorRect == null ? layoutManager.getPaddingTop() : anchorRect.top,
                 anchorRect == null ? layoutManager.getPaddingRight() : anchorRect.right,
-                anchorRect == null ? layoutManager.getPaddingBottom() : anchorRect.bottom
-        );
+                anchorRect == null ? layoutManager.getPaddingBottom() : anchorRect.bottom);
+    }
+
+    public ILayouter getDownLayouter(@Nullable Rect anchorRect) {
+        Rect offsetRect = createOffsetRectForDownLayouter(anchorRect);
 
         AbstractLayouter layouter = new RTLDownLayouter(layoutManager,
                 layoutManager.getChildGravityResolver(),
