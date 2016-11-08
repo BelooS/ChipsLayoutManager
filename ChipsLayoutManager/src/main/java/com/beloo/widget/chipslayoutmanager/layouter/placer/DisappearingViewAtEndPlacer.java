2c69f42bb72b834960af5a1f8cc9c93729207a49
Beloo <Awful.Vi@yandex.ru>
2016-11-08 17:17:44 +0200

    + android arsenal badge for readme

--- ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/layouter/placer/DisappearingViewAtEndPlacer.java
+++ ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/layouter/placer/DisappearingViewAtEndPlacer.java
@@ -17,5 +17,8 @@ class DisappearingViewAtEndPlacer extends AbstractPlacer {
     @Override
     public void addView(View view) {
         getLayoutManager().addDisappearingView(view);
+
+        Timber.i("added disappearing view, position = " + getLayoutManager().getPosition(view));
+//        Timber.d("name = " + ((TextView)view.findViewById(R.id.tvName)).getText().toString());
     }
 }
--- ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/layouter/placer/RealAtEndPlacer.java
+++ ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/layouter/placer/RealAtEndPlacer.java
@@ -3,6 +3,8 @@ package com.beloo.widget.chipslayoutmanager.layouter.placer;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import timber.log.Timber;
+
 class RealAtEndPlacer extends AbstractPlacer implements IPlacer {
     public RealAtEndPlacer(RecyclerView.LayoutManager layoutManager) {
         super(layoutManager);
@@ -11,5 +13,7 @@ class RealAtEndPlacer extends AbstractPlacer implements IPlacer {
     @Override
     public void addView(View view) {
         getLayoutManager().addView(view);
+
+        Timber.i("added view, position = " + getLayoutManager().getPosition(view));
     }
 }
