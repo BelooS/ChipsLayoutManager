df28360029f737a77790833c4dedc4418c6f33df
Beloo <Awful.Vi@yandex.ru>
2016-11-16 18:44:21 +0200

    * successfully finished script with sources, javadoc and without classifier

--- ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/DeviceOrientation.java
+++ ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/DeviceOrientation.java
@@ -3,5 +3,9 @@ package com.beloo.widget.chipslayoutmanager;
 import android.content.res.Configuration;
 import android.support.annotation.IntDef;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 @IntDef({Configuration.ORIENTATION_LANDSCAPE, Configuration.ORIENTATION_PORTRAIT, Configuration.ORIENTATION_UNDEFINED})
+@Retention(RetentionPolicy.SOURCE)
 @interface DeviceOrientation {}
--- ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/SpanLayoutChildGravity.java
+++ ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/SpanLayoutChildGravity.java
@@ -3,5 +3,9 @@ package com.beloo.widget.chipslayoutmanager;
 import android.support.annotation.IntDef;
 import android.view.Gravity;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 @IntDef({android.view.Gravity.TOP, android.view.Gravity.BOTTOM, Gravity.CENTER, Gravity.CENTER_VERTICAL})
+@Retention(RetentionPolicy.SOURCE)
 public @interface SpanLayoutChildGravity {}
