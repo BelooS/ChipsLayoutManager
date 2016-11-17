8c9c39888dd21350c4da0b42703b96b2088c839b
Beloo <Awful.Vi@yandex.ru>
2016-11-17 18:36:11 +0200

    * square updated with right paddings

--- ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/layouter/Square.java
+++ ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/layouter/Square.java
@@ -2,11 +2,11 @@ package com.beloo.widget.chipslayoutmanager.layouter;
 
 import com.beloo.widget.chipslayoutmanager.ChipsLayoutManager;
 
-public class Square implements ICanvas {
+class Square implements ICanvas {
 
     private ChipsLayoutManager chipsLayoutManager;
 
-    public Square(ChipsLayoutManager chipsLayoutManager) {
+    Square(ChipsLayoutManager chipsLayoutManager) {
         this.chipsLayoutManager = chipsLayoutManager;
     }
 
@@ -15,16 +15,15 @@ public class Square implements ICanvas {
     }
 
     public final int getCanvasBottomBorder() {
-        return chipsLayoutManager.getHeight();
+        return chipsLayoutManager.getHeight() - chipsLayoutManager.getPaddingBottom();
     }
 
     public final int getCanvasLeftBorder() {
         return chipsLayoutManager.getPaddingLeft();
     }
 
-    //todo why zero here but not top padding?
     public final int getCanvasTopBorder() {
-        return 0;
+        return chipsLayoutManager.getPaddingTop();
     }
 
 }
