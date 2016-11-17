1d848bccf7eb1f884dc1fbf3fed822e0dd8d7418
Beloo <Awful.Vi@yandex.ru>
2016-11-17 15:21:00 +0200

    + vertical forward layouter

--- ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/layouter/VerticalForwardLayouter.java
+++ ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/layouter/VerticalForwardLayouter.java
@@ -5,28 +5,49 @@ import android.view.View;
 
 public class VerticalForwardLayouter extends AbstractLayouter {
 
+    private boolean isPurged;
+
     VerticalForwardLayouter(Builder builder) {
         super(builder);
     }
 
     @Override
     Rect createViewRect(View view) {
-        return null;
+        Rect viewRect = new Rect(viewLeft, viewTop, viewLeft + getCurrentViewWidth(), viewTop + getCurrentViewHeight());
+
+        viewTop = viewRect.bottom;
+        viewRight = Math.max(viewRight, viewRect.right);
+        return viewRect;
     }
 
     @Override
     void onPreLayout() {
+        if (!rowViews.isEmpty()) {
+            //todo this isn't great place for that. Should be refactored somehow
+            if (!isPurged) {
+                isPurged = true;
+                getCacheStorage().purgeCacheFromPosition(getLayoutManager().getPosition(rowViews.get(0).second));
+            }
 
+            //cache only when go down
+            getCacheStorage().storeRow(rowViews);
+        }
     }
 
     @Override
     void onAfterLayout() {
-
+        //go to next column, increase left coordinate, reset top
+        viewLeft = getViewRight();
+        viewTop = getCanvasTopBorder();
     }
 
     @Override
     boolean isAttachedViewFromNewRow(View view) {
-        return false;
+        int leftOfCurrentView = getLayoutManager().getDecoratedLeft(view);
+        int topOfCurrentView = getLayoutManager().getDecoratedTop(view);
+
+        return viewRight <= leftOfCurrentView
+                && topOfCurrentView < viewTop;
     }
 
     @Override
@@ -36,6 +57,8 @@ public class VerticalForwardLayouter extends AbstractLayouter {
 
     @Override
     void onInterceptAttachView(View view) {
-
+        viewTop = getLayoutManager().getDecoratedBottom(view);
+        viewLeft = getLayoutManager().getDecoratedLeft(view);
+        viewRight = Math.max(viewRight, getLayoutManager().getDecoratedRight(view));
     }
 }
