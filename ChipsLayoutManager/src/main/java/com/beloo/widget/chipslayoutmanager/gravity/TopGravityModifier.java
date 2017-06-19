4804fd8a382eabe710843b44cc4405e1f5b9a287
Mitchell Skaggs <skaggsm333@gmail.com>
2017-06-19 18:47:29 +0300

    Fixed copy-paste error by aligning TopGravityModifier with BottomGravityModifier
    (cherry picked from commit 1a16d51)

--- ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/gravity/TopGravityModifier.java
+++ ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/gravity/TopGravityModifier.java
@@ -3,23 +3,20 @@ package com.beloo.widget.chipslayoutmanager.gravity;
 import android.graphics.Rect;
 
 class TopGravityModifier implements IGravityModifier {
-
     @Override
     public Rect modifyChildRect(int minStart, int maxEnd, Rect childRect) {
-        if (childRect.left < minStart) {
+        if (childRect.top < minStart) {
             throw new IllegalArgumentException("top point of input rect can't be lower than minTop");
         }
-        if (childRect.right > maxEnd) {
+        if (childRect.bottom > maxEnd) {
             throw new IllegalArgumentException("bottom point of input rect can't be bigger than maxTop");
         }
+        Rect modified = new Rect(childRect);
 
-        childRect = new Rect(childRect);
-
-        if (childRect.top > minStart) {
-            childRect.bottom -= (childRect.top - minStart);
-            childRect.top = minStart;
+        if (modified.top > minStart) {
+            modified.bottom -= (modified.top - minStart);
+            modified.top = minStart;
         }
-
-        return childRect;
+        return modified;
     }
 }
